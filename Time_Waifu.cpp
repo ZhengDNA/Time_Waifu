9fef33ed34fd01fd662132ef95cf50d73ab844b6
AmatsukazeDNA <1007324849@qq.com>
2021-02-23 16:04:01 +0800

    source file

--- Time_Waifu.cpp
+++ Time_Waifu.cpp
@@ -3,10 +3,11 @@
  *  EasyX版本	20200902
  */
 
+#include<stdio.h>
 #include<Windows.h>
 #include<graphics.h>
 #include<time.h>
-//mci接口必须
+ //mci接口必须
 #pragma comment(lib,"Winmm.lib")
 
 //mci接口的参数
@@ -18,33 +19,6 @@
 //报时的时间（分：十位，分：个位，秒：十位，秒：个位）
 #define ONTIME t[14]=='0'&&t[15]=='0'&&t[17]=='0'&&t[18]=='0'
 
- //台词文本
-TCHAR word[24][100] = {
-	L"午夜0点。\n新的一天呢。",
-	L"〇一〇〇。\n哈欠…我也稍微睡会吧。你还不去休息么？",
-	L"〇二〇〇。\n不知道初风睡了没有？雪风睡得正香呢，嗯。",
-	L"〇三〇〇。\n你…今天不会打算通宵吧？（我）可不陪你哟。",
-	L"〇四〇〇。\n说过不陪你了…真犟…这都早上了。",
-	L"〇五〇〇。\n看外面啦。已经早上啦。真是！啊啊…真是的…！",
-	L"〇六〇〇。\n话说回来，清晨的风吹拂着心情舒畅…呐，你觉得呢？",
-	L"〇七〇〇。\n…神清气爽的风呢。呐，稍微去海边走走么？",
-	L"〇八〇〇。\n…来咯，早餐准备好了哟。日式的没问题吧？",
-	L"〇九〇〇。\n差不多该准备舰队的正式出航了。你打算怎么安排？",
-	L"一〇〇〇。\n岛风！？…嗯，是呢。算、算是朋友啊。…怎么？干嘛这种表情看着我？",
-	L"一一〇〇。\n什么？连装炮君？可爱吧，一直乖乖的坐在舰首。",
-	L"一二〇〇。\n午餐吃咖喱饭怎么样呢？这可是我的拿手菜哟？真的哟？",
-	L"一三〇〇。\n我做的咖喱饭味道如何？嗯嗯？那是当然！",
-	L"一四〇〇。\n我和岛风谁的速度更快？我又没和她比过不好说啊。",
-	L"一五〇〇。\n因为我是原型舰。所以用我做过很多实验的。速度绝对没有问题的哟。",
-	L"一六〇〇。\n奇怪，连装炮君有点闹情绪…怎么回事？",
-	L"一七〇〇。\n晚风也是很舒服呢。…过来这边、陪我一起看夕阳好么？",
-	L"一八〇〇。\n快到晚餐的时间呢。烧鱼怎么样？",
-	L"一九〇〇。\n…给，晚餐。…怎么样，好吃么…是么，那就好！",
-	L"二〇〇〇。\n啊，岛风很精神嘛？哎，什么！？现在比试！？不要啦已经晚上了！",
-	L"二一〇〇。\n…所以说啦现在不想比试了啦！岛风你很烦啊啊！",
-	L"二二〇〇。\n终、终于回去了。真是的…想干什么啊！",
-	L"二三〇〇。\n今天就要结束了。辛苦你了，明天见。"
-};
 //设置字体样式
 void textStyle(const TCHAR* fontFamily, int fontSize, COLORREF fontColor)
 {
@@ -60,14 +34,29 @@ void textStyle(const TCHAR* fontFamily, int fontSize, COLORREF fontColor)
 void displayWords(char h1, char h2)
 {
 	RECT prect = { TEXTAREA };		//文字显示范围
-	textStyle(L"微软雅黑", 30, RGB(186, 32, 32));
-	drawtext(word[h1 * 10 + h2], &prect, DT_WORDBREAK);
+	textStyle(L"微软雅黑", 30, RGB(186, 32, 32));	
+	char fileName[13] = "words/00.txt";
+	fileName[6] = h1, fileName[7] = h2;		//确定文件名
+	FILE* fp;
+	fopen_s(&fp, fileName, "r");
+	char words[100] = {0};
+	int i;
+	char ch;
+	for (i = 0, ch = fgetc(fp); ch != EOF; i++, ch = fgetc(fp))
+	{
+		words[i] = ch;
+	}
+	words[i] = '\0';	//读取台词文件的内容
+	TCHAR wWords[100];
+	MultiByteToWideChar(CP_ACP, 0, words, -1, wWords, 100);	//转换为宽字符
+	drawtext(wWords, &prect, DT_WORDBREAK);	//显示台词
+	fclose(fp);
 }
 //显示音量按键
 void volumeText(int volume)
 {
 	textStyle(L"微软雅黑", 30, RGB(186, 32, 32));
-	TCHAR volumetext[] = _T("音量 7");
+	TCHAR volumetext[] = L"音量 7";
 	volumetext[3] = (volume % 8) + 48;
 	outtextxy(560, 440, volumetext);
 }
@@ -92,7 +81,7 @@ void timeVoice(char h1, char h2, int volume)
 	mciSendStringA(a, DETAILS);		//打开音频文件
 	changeVolume(volume);
 	mciSendStringA("play voice", DETAILS);
-	displayWords(h1 - 48, h2 - 48);
+	displayWords(h1, h2);
 }
 //主函数
 int main()
@@ -171,10 +160,7 @@ int main()
 			}
 		}
 		FlushMouseMsgBuffer();
-		Sleep(50);
+		Sleep(50);	//根据计算机性能调整冻结时间
 	}
-
-	system("pause");
-	closegraph();
 	return 0;
 }
